9962319b58f5a201536e38f90f7356de2b98f230
KK <karanjakelvin77@gmail.com>
2022-03-07 21:47:52 +0300

    It will initialize db

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
-from ensurepip import bootstrap
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
 
@@ -12,6 +13,7 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
     bootstrap.init_app(app)
+    db.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
