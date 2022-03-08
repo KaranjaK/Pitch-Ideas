31e82ab452faf6dac020b4be1049106ccd4fd16c
KK <karanjakelvin77@gmail.com>
2022-03-08 14:04:40 +0300

    It will add the role model

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255))
     password = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return f'User {self.username}'
@@ -15,3 +16,15 @@ class Pitches(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     pitch = db.Column(db.String)
     pitcher = db.column(db.String)
+
+    def __repr__(self):
+        return f'User {self.pitch}'
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def __repr__(self):
+        return f'User {self.name}'
