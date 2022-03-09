ecde81168c2da2a7e56eb92c76e6d6ad314d3af2
KK <karanjakelvin77@gmail.com>
2022-03-09 16:28:15 +0300

    It will update authentification

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField, TextAreaField
+from wtforms import StringField,PasswordField,SubmitField, TextAreaField, SelectField
 from wtforms.validators import InputRequired,Email,EqualTo
 from ..models import Users
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
+    bio = TextAreaField('Tell us about yourself:',validators = [InputRequired()])
     submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired()])
+    category = SelectField('Category', choices=[('Events','Events'),('Job','Job'),('Advertisement','Advertisement')],validators=[Required()])
+    post = TextAreaField('Your Pitch', validators=[InputRequired()])
+    submit = SubmitField('Pitch')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Leave a comment',validators=[InputRequired()])
+    submit = SubmitField('Comment')
