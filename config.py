dd4cdaaa2a3f537d70344a6451d82eec4b63b8ac
KK <karanjakelvin77@gmail.com>
2022-03-11 16:31:53 +0300

    Heroku Deployment

--- config.py
+++ config.py
@@ -3,17 +3,17 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kk:admin@localhost/pitches'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465 
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL= True
-    MAIL_USERNAME = 'appskelvin5@gmail.com'
-    MAIL_PASSWORD = 'sjyhfgizwssnlttl'
-    SIMPLEMDE_JS_IIFE = True
-    SIMPLEMDE_USE_CDN = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    UPLOADED_PHOTOS_DEST='app/static/photos'
+    MAIL_SERVER='smtp.gmail.com'
+    MAIL_PORT=465 
+    MAIL_USE_TLS=False
+    MAIL_USE_SSL=True
+    MAIL_USERNAME='appskelvin5@gmail.com'
+    MAIL_PASSWORD='sjyhfgizwssnlttl'
+    SIMPLEMDE_JS_IIFE=True
+    SIMPLEMDE_USE_CDN=True
 
 class DevConfig(Config):
     DEBUG = True
